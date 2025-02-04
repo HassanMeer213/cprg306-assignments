8a736547d88c6d87c565b7d37a7601c65cb6cf8a
Hassan Mir <h4ss4nmeer345@gmail.com>
2025-02-04 05:51:24 -0700

    change to code

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,6 +1,8 @@
+'use client';
+
 export default function Item({ name, quantity, category }) {
   return (
-    <li className="p-2 border-b border-gray-300 text-black">
+    <li className="p-2 border-b border-gray-300">
       <span className="font-bold">{name}</span> - Quantity: {quantity}, Category: {category}
     </li>
   );
